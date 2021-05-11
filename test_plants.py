67c55f6fce016599a369651844e1a0abc68e6b20
Navz19 <jdnaviad@unal.edu.co>
2021-05-11 12:53:39 -0500

    Ampliar cobertura de prueba al 100%

--- test_plants.py
+++ test_plants.py
@@ -1,7 +1,16 @@
+import pytest
 from plants import COLOMBIAN_PLANTS
 from plants import get_random_plant
 
 def test_successfully_get_random_plant():
     random_plant = get_random_plant()
-    assert type(random_plant) == str
-    assert (random_plant in COLOMBIAN_PLANTS) is True
+    assert isinstance(random_plant, str)
+    assert any(random_plant in T for T in COLOMBIAN_PLANTS.values())
+    for key in COLOMBIAN_PLANTS.keys():
+        random_plant = get_random_plant(key)
+        assert isinstance(random_plant, str)
+        assert any(random_plant in T for T in COLOMBIAN_PLANTS.values())
+    
+def test_unsuccesful_get_random_plant():
+    with pytest.raises(KeyError):
+        get_random_plant('Azul')
