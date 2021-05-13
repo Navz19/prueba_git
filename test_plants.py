6460c3d9b160cad45a74f01c5ec1295dc5921f85
Navz19 <jdnaviad@unal.edu.co>
2021-05-13 16:57:14 -0500

    Ajuste de las pruebs unitarias para cada caso tipo de planta y caso cuando tipo no está en lista

--- test_plants.py
+++ test_plants.py
@@ -1,16 +1,19 @@
 import pytest
-from plants import COLOMBIAN_PLANTS
+from plants import COLOMBIAN_PLANTS, PlantTypeDoesNotExistsError
 from plants import get_random_plant
 
 def test_successfully_get_random_plant():
     random_plant = get_random_plant()
     assert isinstance(random_plant, str)
-    assert any(random_plant in T for T in COLOMBIAN_PLANTS.values())
-    for key in COLOMBIAN_PLANTS.keys():
-        random_plant = get_random_plant(key)
+    assert any(random_plant in plant_type_list for plant_type_list in COLOMBIAN_PLANTS.values())
+
+def test_successfully_get_random_plant_by_type():
+    for plant_type in COLOMBIAN_PLANTS.keys():
+        random_plant = get_random_plant(plant_type)
         assert isinstance(random_plant, str)
-        assert any(random_plant in T for T in COLOMBIAN_PLANTS.values())
+        assert any(random_plant in plant_type_list for plant_type_list in COLOMBIAN_PLANTS.values())
     
-def test_unsuccesful_get_random_plant():
-    with pytest.raises(KeyError):
-        get_random_plant('Azul')
+def test_get_random_plant_invalid_type():
+    unregistered_plant_type = "Azul"
+    with pytest.raises(PlantTypeDoesNotExistsError):
+        get_random_plant(unregistered_plant_type)
